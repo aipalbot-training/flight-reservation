a984ae6f2e4f007e501ec3b7f10b5531c5c92581
aipalbot-raas <info@aipalbot.com>
2020-08-05 20:55:29 -0400

    decopupling started

--- flight.reservation/src/main/java/com/aipalbot/flight/reservation/services/AbstractFlightService.java
+++ flight.reservation/src/main/java/com/aipalbot/flight/reservation/services/AbstractFlightService.java
@@ -2,7 +2,10 @@ package com.aipalbot.flight.reservation.services;
 
 import com.aipalbot.flight.reservation.interfaces.IPaymentService;
 import com.aipalbot.flight.reservation.models.Card;
+import com.aipalbot.flight.reservation.models.Flight;
+import com.aipalbot.flight.reservation.models.Passenger;
 import com.aipalbot.flight.reservation.models.Person;
+import com.aipalbot.flight.reservation.models.Pet;
 import com.aipalbot.flight.reservation.models.Receipt;
 
 public abstract class AbstractFlightService implements IPaymentService{
@@ -35,6 +38,29 @@ public abstract class AbstractFlightService implements IPaymentService{
 	public boolean verifyCar(Card card) {
 		
 		return false;
+	}
+
+	@Override
+	public Passenger reserveSeat(Person person, Pet pet, String luggage, String flightName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String showAvailableSeats(Flight flight) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String showReservedSeats(Flight flight) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateBalance(Card card, double money) {
+		// TODO Auto-generated method stub		
 	}
 	
 }
